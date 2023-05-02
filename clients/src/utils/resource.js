1eeb697acc2acdb49bf4df7d4081453cd31f14ef
Peter Jolley <peter@marango.com.au>
2023-05-02 12:29:48 +0930

    formatting and utils updated

--- clients/src/utils/resource.js
+++ clients/src/utils/resource.js
@@ -1,4 +1,50 @@
 import { toast } from "react-toastify";
+import emailjs from "emailjs/browser";
+
+export const time = [
+    {id:"null", t:"Select"},
+    {id:"7", t:"7:00 AM"},
+    {id:"8", t:"8:00 AM"},
+    {id:"9", t:"9:00 AM"},
+    {id:"10", t:"10:00 AM"},
+    {id:"11", t:"11:00 AM"},
+    {id:"12", t:"12:00 PM"},
+    {id:"13", t:"1:00 PM"},
+    {id:"14", t:"2:00 PM"},
+    {id:"15", t:"3:00 PM"},
+    {id:"16", t:"4:00 PM"},
+    {id:"17", t:"5:00 PM"},
+    {id:"18", t:"6:00 PM"},
+    {id:"19", t:"7:00 PM"},
+];
+
+export async function handleLogin(username,password, navigate){
+    try{
+        const request = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await request.json();
+        if(data.error_message){
+            toast.error(data.error_message);
+        } else {
+            toast.success(data.message);
+            localStorage.setItem("_id", data.data._id);
+            localStorage.setItem("_myEmail", data.data._email);
+            navigate("/dashboard");
+        }
+    } catch(err) {
+        console.log(err);
+        
+    }
+}
 
 export async function handleRegister(email,username,password){
     try {
@@ -27,33 +73,7 @@ export async function handleRegister(email,username,password){
     toast.error("Account creation failed");
 }
 }
-export async function handleLogin(username,password, navigate){
-    try{
-        const request = await fetch("http://localhost:4000/login", {
-            method: "POST",
-            body: JSON.stringify({
-                username,
-                password,
-            }),
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-        });
-        const data = await request.json();
-        if(data.error_message){
-            toast.error(data.error_message);
-        } else {
-            toast.success(data.message);
-            localStorage.setItem("_id", data.data._id);
-            localStorage.setItem("_myEmail", data.data._email);
-            navigate("/dashboard");
-        }
-    } catch(err) {
-        console.log(err);
-        
-    }
-}
+
 export async function handleCreateSchedule(selectedTimezone, schedule, navigate){
     try{
         const request = await fetch("http://localhost:4000/schedule/create", {
@@ -105,19 +125,35 @@ export async function fetchBookingDetails(
     .catch((err) => console.error(err));
 }
 
-export const time = [
-    {id:"null", t:"Select"},
-    {id:"7", t:"7:00 AM"},
-    {id:"8", t:"8:00 AM"},
-    {id:"9", t:"9:00 AM"},
-    {id:"10", t:"10:00 AM"},
-    {id:"11", t:"11:00 AM"},
-    {id:"12", t:"12:00 PM"},
-    {id:"13", t:"1:00 PM"},
-    {id:"14", t:"2:00 PM"},
-    {id:"15", t:"3:00 PM"},
-    {id:"16", t:"4:00 PM"},
-    {id:"17", t:"5:00 PM"},
-    {id:"18", t:"6:00 PM"},
-    {id:"19", t:"7:00 PM"},
-];
+export const sendEmail= (
+    receiverEmail,
+    email,
+    fullName,
+    message,
+    duration
+) => {
+    emailjs
+    .send(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        {
+            to_email: receiverEmail,
+            from_email: email,
+            fullName,
+            message,
+            duration,
+        },
+        "YOUR_PUBLIC_KEY"
+    )
+    .then(
+        (result) => {
+            console.log(result.text);
+            toast.success("Session booked successfully");
+        },
+        (error) => {
+            console.log(error.text);
+            toast.error(error.text);
+        }
+    );
+};
+
